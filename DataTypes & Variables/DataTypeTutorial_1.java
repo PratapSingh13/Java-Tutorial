7cc650da8d744bb0dc3027ad22d9e5717ef8206b
yogendra.pratap <yogendra.pratap@opstree.com>
2020-05-21 16:59:41 +0530

    DataTypes & Variables file added

---
+++ DataTypes & Variables/DataTypeTutorial_1.java
@@ -0,0 +1,22 @@
+/*This Program is for Data type tutorial*/
+
+public class DataTypeTutorial_1
+{
+	public static void main(String[] args) 
+	{
+		byte num = 2;
+		System.out.println("num = " +num);
+		
+		int age = 24;
+		System.out.println("Age = " +age);
+		
+		float rateOfInterest = 2.5f;
+		double valueOfPie = 3.1428571;
+		System.out.println("Rate of Interest = " +rateOfInterest);
+		System.out.println("Value of Pie = " +valueOfPie);
+		
+		boolean isThisSeriesCool = true;
+		System.out.println("Is This Series is Cool: " +isThisSeriesCool);
+	}
+
+}
